b0192054a8cf771441f0a82e78043812feef783f
Ana Caroline W. M <121321103+Carolwm@users.noreply.github.com>
2024-03-18 20:49:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// 01 - Crie uma variável chamada idade e atribua a ela o valor 35 e exiba o seu valor.
+
+using System.Runtime.InteropServices;
+
+int idade = 35;
+Console.WriteLine(idade);
+
+// 02 - Crie uma variável chamada nome e atribua o valor "Maria" e exiba o seu valor.
+
+string nome = "maria";
+Console.WriteLine(nome);
+
+// 03 - Crie uma variável chamada altura e atribua a ela o valor 3.45.
+
+float altura = 3.45f;
+Console.WriteLine(altura);
+
+//04 - Crie uma constante chamada ano e atribua a ela o valor 12 e exiba o seu valor
+
+const int ano = 12;
+Console.WriteLine(ano);
+
+// 05 - Declare uma variável nota do tipo double como sendo do tipo Nullable type e atribua o
+//valor 7.80 e exiba o seu valor
+
+double? nota = 7.80;
+Console.WriteLine(nota);
+
+// 10 - Declare duas variáveis x e y como sendo do tipo int e atribua a ela os valores 77 e 66 e a
+//seguir imprima o valor da soma de x com y. (Use o operador aritmético + para realizar a
+//operação de soma)
+
+int x = 77;
+int y = 66;
+int soma = x+y;
+Console.WriteLine(soma);
+
+
+
+
+
+
+
+
+
